38e18d896a11676728c70548704f801d848af9b7
Emanuel Peire <emapeire@outlook.com>
2023-11-09 12:50:44 -0300

    initial tic tac toe

---
+++ src/App.jsx
@@ -0,0 +1,92 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+import "./App.css";
+
+const TURNS = {
+  X: "X",
+  O: "O",
+};
+
+const Square = ({ children, updateBoard, index, isSelected }) => {
+  const className = `square ${isSelected ? "is-selected" : ""}`;
+  const handleClick = () => {
+    updateBoard(index);
+  };
+
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  );
+};
+
+const WINNER_COMBINATIONS = [
+  // Horizontal
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+
+  // Vertical
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+
+  // Diagonal
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+export default function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [turn, setTurn] = useState(TURNS.X);
+  const [winner, setWinner] = useState(null);
+
+  const checkWinner = (boardToCheck) => {
+    for (const combination of WINNER_COMBINATIONS) {
+      const [a, b, c] = combination;
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a];
+      }
+    }
+    return null;
+  };
+
+  const updateBoard = (index) => {
+    if (board[index] || winner) return;
+
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
+
+    const newWinner = checkWinner(newBoard);
+    if (newWinner) {
+      setWinner(newWinner);
+    }
+  };
+
+  return (
+    <main className="board">
+      <h1>Tic Tac Toe</h1>
+      <section className="game">
+        {board.map((_cell, index) => {
+          return (
+            <Square key={index} index={index} updateBoard={updateBoard}>
+              {board[index]}
+            </Square>
+          );
+        })}
+      </section>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+      </section>
+    </main>
+  );
+}
